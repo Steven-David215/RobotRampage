d184b54153b261c079c0625083eeae0f16e475b9
Steven 'SteveDave' David <stevedave215@gmail.com>
2021-10-20 10:54:35 -0400

    Chapter 9 - Ammunition logic

---
+++ Assets/Scripts/Pistol.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol : Gun
+{
+    override protected void Update()
+    {
+        base.Update();
+
+        //Shotgun and pistol have semi-auto fire rate
+
+        if(Input.GetMouseButtonDown(0) && (Time.time - lastFireTime) > fireRate)
+        {
+            lastFireTime = Time.time;
+            Fire();
+        }
+    }
+}
---
+++ Assets/Scripts/Shotgun.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shotgun : Gun
+{
+    override protected void Update()
+    {
+        base.Update();
+
+        //Shotgun and pistol have semi-auto fire rate
+
+        if(Input.GetMouseButtonDown(0) && (Time.time - lastFireTime) > fireRate)
+        {
+            lastFireTime = Time.time;
+            Fire();
+        }
+    }
+}

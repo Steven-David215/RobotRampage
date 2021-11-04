000c8c1766c614d73bdbb7d9c61409ff42d9057b
Steven 'SteveDave' David <stevedave215@gmail.com>
2021-11-03 20:57:35 -0400

    Chapter 10 - More Spawning logic.

---
+++ Assets/Scripts/PickupSpawn.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupSpawn : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] pickups;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPickup();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void spawnPickup()
+    {
+        GameObject pickup = Instantiate(pickups[Random.Range(0, pickups.Length)]);
+        pickup.transform.position = transform.position;
+        pickup.transform.parent = transform;
+    }
+
+    IEnumerator respawnPickup()
+    {
+        yield return new WaitForSeconds(20);
+        spawnPickup();
+    }
+
+    public void PickupWasPickedUp()
+    {
+        StartCoroutine("respawnPickup");
+    }
+}
